19c9630d8e8a273455abb4f5d47ed9919a7ac69e
Brandon Kang <myfriendkang@gmail.com>
2017-08-15 10:13:50 -0700

    just empty space

--- app.js
+++ app.js
@@ -144,8 +144,6 @@ myPort.on('error', function(data){
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
